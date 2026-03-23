99ae392bca876f3434697655fe0ef66eef0a65de
Jayampathy Balasuriya <j2damax@gmail.com>
2026-03-23 11:56:04 +0530

    project scaffolding

---
+++ SeeSaw/SeeSawTests/SeeSawTests.swift
@@ -0,0 +1,16 @@
+//
+//  SeeSawTests.swift
+//  SeeSawTests
+//
+//  Created by Jayampathy Balasuriya on 2026-03-23.
+//
+
+import Testing
+
+struct SeeSawTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
